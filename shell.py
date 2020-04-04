556dd9ebc783fa5bda16d410c8f709323da9f4d0
Soumalya1857 <soumalyakundu10.gmail.com>
2020-04-04 09:41:25 +0530

    comment added

--- shell.py
+++ shell.py
@@ -6,6 +6,7 @@ while True:
 
     if error != None:
         #print("it's an error")
+        #adding this comment just to initialize git
         print(error.as_string())
     else:
         #print("it's not an error....its a ast")
