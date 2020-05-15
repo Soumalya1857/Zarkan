be64b5c95013aed451cdfab18737f582e3d43659
Soumalya1857 <soumalyakundu10.gmail.com>
2020-05-15 22:04:40 +0530

    symbolTable updated

--- interpreter.py
+++ interpreter.py
@@ -113,6 +113,11 @@ class Interpreter:
 
 global_symbol_table = SymbolTable()
 global_symbol_table.set("null",Number(0))
+global_symbol_table.set("NULL",Number(0))
+global_symbol_table.set("TRUE",Number(1))
+global_symbol_table.set("FALSE",Number(0))
+global_symbol_table.set("true",Number(1))
+global_symbol_table.set("false",Number(0))
 
 
 def run(fn, text):
