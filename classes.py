96276e2f00f27644d7433210cd0550ee6231e6f2
Soumalya1857 <soumalyakundu10.gmail.com>
2020-05-17 21:53:38 +0530

    classes updted and refactored

--- classes.py
+++ classes.py
@@ -1,6 +1,7 @@
 
 
 from error import *
+from interpreter import *
 #######################################
 # POSITION
 #######################################
@@ -197,12 +198,156 @@ class CallNode:
 			self.pos_end = self.node_to_call.pos_end
 
 
+
+#######################################
+# RUNTIME RESULT
+#######################################
+
+
+class RTResult:
+	def __init__(self):
+		self.value = None
+		self.error = None
+
+	def register(self, res):
+		if res.error: self.error = res.error
+		return res.value
+
+	def success(self, value):
+		self.value = value
+		return self
+
+	def failure(self, error):
+		self.error = error
+		return self
+
+
+#######################################
+# PARSE RESULT
+#######################################
+
+
+class ParseResult:
+	def __init__(self):
+		self.error = None
+		self.node = None
+		self.advance_count = 0 # count number of advancements in particular expr
+
+	def register_advancement(self):
+		self.advance_count += 1
+
+	def register(self, res):
+		self.advance_count += res.advance_count
+		if res.error: self.error = res.error
+		return res.node
+
+
+	def success(self, node):
+		self.node = node
+		return self
+
+	def failure(self, error):
+		if not self.error or self.advance_count == 0:
+			self.error = error
+		return self
+
+
 #######################################
 # VALUES
 #######################################
-class Number:
-	def __init__(self, value):
-		self.value = value
+# class Number:
+# 	def __init__(self, value):
+# 		self.value = value
+# 		self.set_pos()
+# 		self.set_context()
+
+# 	def set_pos(self, pos_start=None, pos_end=None):
+# 		self.pos_start = pos_start
+# 		self.pos_end = pos_end
+# 		return self
+# 	def set_context(self,context = None):
+# 		self.context = context
+# 		return self
+
+# 	def added_to(self, other):
+# 		if isinstance(other, Number):
+# 			return Number(self.value + other.value).set_context(self.context), None
+
+# 	def subbed_by(self, other):
+# 		if isinstance(other, Number):
+# 			return Number(self.value - other.value).set_context(self.context), None
+
+# 	def multed_by(self, other):
+# 		if isinstance(other, Number):
+# 			return Number(self.value * other.value).set_context(self.context), None
+
+# 	def dived_by(self, other):
+# 		if isinstance(other, Number):
+# 			if other.value == 0:
+# 				return None, RTError(
+# 					other.pos_start, other.pos_end,
+# 					'Division by zero',
+# 					self.context
+# 				)
+
+# 			return Number(self.value / other.value).set_context(self.context), None
+
+# 	def powed_by(self, other):
+# 		if isinstance(other, Number):
+# 			return Number(self.value ** other.value).set_context(self.context), None
+
+# 	def get_comparison_eq(self,other):
+# 		if isinstance(other, Number):
+# 			return Number(int(self.value == other.value)).set_context(self.context),None
+
+# 	def get_comparison_ne(self,other):
+# 		if isinstance(other, Number):
+# 			return Number(int(self.value != other.value)).set_context(self.context),None
+
+# 	def get_comparison_lt(self,other):
+# 		if isinstance(other, Number):
+# 			return Number(int(self.value < other.value)).set_context(self.context),None
+
+# 	def get_comparison_gt(self,other):
+# 		if isinstance(other, Number):
+# 			return Number(int(self.value > other.value)).set_context(self.context),None
+
+# 	def get_comparison_lte(self,other):
+# 		if isinstance(other, Number):
+# 			return Number(int(self.value <= other.value)).set_context(self.context),None
+
+# 	def get_comparison_gte(self,other):
+# 		if isinstance(other, Number):
+# 			return Number(int(self.value >= other.value)).set_context(self.context),None
+
+# 	def anded_by(self,other):
+# 		if isinstance(other, Number):
+# 			return Number(int(self.value and other.value)).set_context(self.context),None
+
+# 	def ored_by(self,other):
+# 		if isinstance(other, Number):
+# 			return Number(int(self.value or other.value)).set_context(self.context),None
+
+# 	def notted(self):
+# 		if isinstance(other,Number):
+# 			return Number(1 if self.value == 0 else 0).set_context(self.context),None
+
+
+# 	def is_true(self):
+# 		return self.value != 0
+
+# 	def copy(self):
+# 		copy = Number(self.value)
+# 		copy.set_pos(self.pos_start, self.pos_end)
+# 		copy.set_context(self.context)
+# 		return copy	
+# 	def __repr__(self):
+# 		return str(self.value)
+
+
+
+class Value:
+	def __init__(self):
 		self.set_pos()
 		self.set_context()
 
@@ -210,21 +355,92 @@ class Number:
 		self.pos_start = pos_start
 		self.pos_end = pos_end
 		return self
-	def set_context(self,context = None):
+
+	def set_context(self, context=None):
 		self.context = context
 		return self
 
 	def added_to(self, other):
+		return None, self.illegal_operation(other)
+
+	def subbed_by(self, other):
+		return None, self.illegal_operation(other)
+
+	def multed_by(self, other):
+		return None, self.illegal_operation(other)
+
+	def dived_by(self, other):
+		return None, self.illegal_operation(other)
+
+	def powed_by(self, other):
+		return None, self.illegal_operation(other)
+
+	def get_comparison_eq(self, other):
+		return None, self.illegal_operation(other)
+
+	def get_comparison_ne(self, other):
+		return None, self.illegal_operation(other)
+
+	def get_comparison_lt(self, other):
+		return None, self.illegal_operation(other)
+
+	def get_comparison_gt(self, other):
+		return None, self.illegal_operation(other)
+
+	def get_comparison_lte(self, other):
+		return None, self.illegal_operation(other)
+
+	def get_comparison_gte(self, other):
+		return None, self.illegal_operation(other)
+
+	def anded_by(self, other):
+		return None, self.illegal_operation(other)
+
+	def ored_by(self, other):
+		return None, self.illegal_operation(other)
+
+	def notted(self):
+		return None, self.illegal_operation(other)
+
+	def execute(self, args):
+		return RTResult().failure(self.illegal_operation())
+
+	def copy(self):
+		raise Exception('No copy method defined')
+
+	def is_true(self):
+		return False
+
+	def illegal_operation(self, other=None):
+		if not other: other = self
+		return RTError(
+			self.pos_start, other.pos_end,
+			'Illegal operation',
+			self.context
+		)
+
+class Number(Value):
+	def __init__(self, value):
+		super().__init__()
+		self.value = value
+
+	def added_to(self, other):
 		if isinstance(other, Number):
 			return Number(self.value + other.value).set_context(self.context), None
+		else:
+			return None, Value.illegal_operation(self, other)
 
 	def subbed_by(self, other):
 		if isinstance(other, Number):
 			return Number(self.value - other.value).set_context(self.context), None
+		else:
+			return None, Value.illegal_operation(self, other)
 
 	def multed_by(self, other):
 		if isinstance(other, Number):
 			return Number(self.value * other.value).set_context(self.context), None
+		else:
+			return None, Value.illegal_operation(self, other)
 
 	def dived_by(self, other):
 		if isinstance(other, Number):
@@ -236,58 +452,124 @@ class Number:
 				)
 
 			return Number(self.value / other.value).set_context(self.context), None
+		else:
+			return None, Value.illegal_operation(self, other)
 
 	def powed_by(self, other):
 		if isinstance(other, Number):
 			return Number(self.value ** other.value).set_context(self.context), None
+		else:
+			return None, Value.illegal_operation(self, other)
 
-	def get_comparison_eq(self,other):
+	def get_comparison_eq(self, other):
 		if isinstance(other, Number):
-			return Number(int(self.value == other.value)).set_context(self.context),None
+			return Number(int(self.value == other.value)).set_context(self.context), None
+		else:
+			return None, Value.illegal_operation(self, other)
 
-	def get_comparison_ne(self,other):
+	def get_comparison_ne(self, other):
 		if isinstance(other, Number):
-			return Number(int(self.value != other.value)).set_context(self.context),None
+			return Number(int(self.value != other.value)).set_context(self.context), None
+		else:
+			return None, Value.illegal_operation(self, other)
 
-	def get_comparison_lt(self,other):
+	def get_comparison_lt(self, other):
 		if isinstance(other, Number):
-			return Number(int(self.value < other.value)).set_context(self.context),None
+			return Number(int(self.value < other.value)).set_context(self.context), None
+		else:
+			return None, Value.illegal_operation(self, other)
 
-	def get_comparison_gt(self,other):
+	def get_comparison_gt(self, other):
 		if isinstance(other, Number):
-			return Number(int(self.value > other.value)).set_context(self.context),None
+			return Number(int(self.value > other.value)).set_context(self.context), None
+		else:
+			return None, Value.illegal_operation(self, other)
 
-	def get_comparison_lte(self,other):
+	def get_comparison_lte(self, other):
 		if isinstance(other, Number):
-			return Number(int(self.value <= other.value)).set_context(self.context),None
+			return Number(int(self.value <= other.value)).set_context(self.context), None
+		else:
+			return None, Value.illegal_operation(self, other)
 
-	def get_comparison_gte(self,other):
+	def get_comparison_gte(self, other):
 		if isinstance(other, Number):
-			return Number(int(self.value >= other.value)).set_context(self.context),None
+			return Number(int(self.value >= other.value)).set_context(self.context), None
+		else:
+			return None, Value.illegal_operation(self, other)
 
-	def anded_by(self,other):
+	def anded_by(self, other):
 		if isinstance(other, Number):
-			return Number(int(self.value and other.value)).set_context(self.context),None
+			return Number(int(self.value and other.value)).set_context(self.context), None
+		else:
+			return None, Value.illegal_operation(self, other)
 
-	def ored_by(self,other):
+	def ored_by(self, other):
 		if isinstance(other, Number):
-			return Number(int(self.value or other.value)).set_context(self.context),None
+			return Number(int(self.value or other.value)).set_context(self.context), None
+		else:
+			return None, Value.illegal_operation(self, other)
 
 	def notted(self):
-		if isinstance(other,Number):
-			return Number(1 if self.value == 0 else 0).set_context(self.context),None
-
-
-	def is_true(self):
-		return self.value != 0
+		return Number(1 if self.value == 0 else 0).set_context(self.context), None
 
 	def copy(self):
 		copy = Number(self.value)
 		copy.set_pos(self.pos_start, self.pos_end)
 		copy.set_context(self.context)
-		return copy	
+		return copy
+
+	def is_true(self):
+		return self.value != 0
+	
 	def __repr__(self):
 		return str(self.value)
+
+
+
+class function(Value):
+	def __init__(self, name, body_node, arg_names):
+		super().__init__()
+		self.name = name
+		self.body_node = body_node
+		self.arg_names = arg_names
+
+	def execute(self,args):
+		res = RTReult()
+		interpreter = interpreter()
+		new_context = Context(self.name, self.context, self.pos_start)
+
+		if len(args) > len(self.arg_names):
+			return res.faliure(RTError(
+				f"{len(args)-len(self.arg_names)} too many args passed into '{self.name}'",
+				self.context
+			))
+
+		if len(args) < len(self.arg_names):
+			return res.faliure(RTError(
+				self.pos_start, self.pos_end,
+				f"{len(self.arg_names)-len(args)} too few args passed into '{self.name}'",
+				self.context
+			))
+
+		for i in range(len(args)):
+			arg_name = self.arg_names[i]
+			arg_value = args[i]
+			arg_value.set_context(new_context)
+			new_context.symbol_table.set(arg_name, arg_value)
+
+		value = res.register(interpreter.visit(self.body, new_context))
+		if res.error: return res
+		return res.success(value)
+
+	def copy(self):
+		copy = Function(self.name, self.body_node, self.arg_names)
+		copy.set_context(self.context)
+		copy.set_pos(self.pos_start, self.pos_end)
+		return copy
+
+	def __repr__(self):
+		return f"<function {self.name}>"
+
 
 
 ######################################
